efbc211f44230004160c3e51170cb3203d260ffc
XavRobo <xavier.lesage@t-online.de>
2020-12-01 17:45:35 +0100

    day1 p2 rem

--- adventofcode/day_1.py
+++ adventofcode/day_1.py
@@ -19,11 +19,16 @@ data.sort()
 #         back += 1
 
 # Part two
+sol = []
+res = []
 for a in range(0,len(data)):
     for b in range(0,len(data)):
         if(b != a and b  > a):
             for c in range(0,len(data)):
                 if(c != a and c!= b and c > b):
                     if(data[a]+data[b]+data[c]==2020):
-                        print(data[a],data[b],data[c])
-                        print(data[a]*data[b]*data[c])
+                        sol.append([data[a],data[b],data[c]])
+                        res.append(data[a]*data[b]*data[c])
+
+print(sol)
+print(res)
