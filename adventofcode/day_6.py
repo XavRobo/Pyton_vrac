65a3d1b86ef651a955fde097a8f41de72c8911a6
XavRobo <xavier.lesage@t-online.de>
2020-12-06 18:32:45 +0100

    day 6 p1

--- adventofcode/day_6.py
+++ adventofcode/day_6.py
@@ -4,12 +4,19 @@ with open("inputs/input6.txt", "r", encoding="utf-8") as f:
 
 counter = 0
 
+# Part One
 for l in L:
-    s = l.split("\n")
-    if(len(s)==1):
-        counter += len(s[0])
-    else:
-        res = set(s[0]).intersection(*[set(e) for e in s[1:]])
-        counter += len(res)
+    s = set(l)
+    s.discard('\n')
+    counter += len(s)
+
+# Part Two
+# for l in L:
+#     s = l.split("\n")
+#     if(len(s)==1):
+#         counter += len(s[0])
+#     else:
+#         res = set(s[0]).intersection(*[set(e) for e in s[1:]])
+#         counter += len(res)
 
 print(counter)
